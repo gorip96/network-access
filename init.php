8850aebccc2adbf00ba289078cb116ebd4a0fa9d
Riv <riv@ixtelecom.net>
2021-07-31 21:08:20 +0800

    clean up init.php

--- init.php
+++ init.php
@@ -1,5 +1,4 @@
 <?php
 require "config.php";
 	$conn = new PDO("mysql:host=$dbhost;dbname=$dbname;port=$dbport", "$dbuser", "$dbpass");
-	// $conn = new PDO('mysql:host=localhost;dbname=engineering', 'engineering', 'IXT3l3c0m');
 ?>
