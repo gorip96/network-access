fec9ead174961afa8e0b076871e2386b4c1e1b06
Riv <riv@ixtelecom.net>
2021-08-03 00:19:40 +0800

    update

--- index.php
+++ index.php
@@ -21,8 +21,10 @@ $_SESSION['LAST_ACTIVITY'] = time(); // update last activity time stamp
   <!-- Bootstrap CSS -->
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0/css/bootstrap.min.css" />
   <link rel="stylesheet" href="main.css">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
   <title>IX Telecom Network Access</title>
 </head>
 
