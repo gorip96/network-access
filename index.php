e03944beaa9a6179463c694ebf7005fb05b95d3c
Riv <riv@ixtelecom.net>
2021-08-02 16:48:41 +0800

    update table

--- index.php
+++ index.php
@@ -47,37 +47,6 @@ if (empty($_SESSION['id'])) {
           </div>
         <?php else: ?>
           <button class="btn btn-lg btn-primary btn-block">I'm verified!!!</button>
-           <div class="container">
-             <h2>Striped Rows</h2>
-             <p>The .table-striped class adds zebra-stripes to a table:</p>            
-             <table class="table table-striped">
-               <thead>
-                 <tr>
-                   <th>Firstname</th>
-                   <th>Lastname</th>
-                   <th>Email</th>
-                 </tr>
-               </thead>
-               <tbody>
-                 <tr>
-                   <td>John</td>
-                   <td>Doe</td>
-                   <td>john@example.com</td>
-                 </tr>
-                 <tr>
-                   <td>Mary</td>
-                   <td>Moe</td>
-                   <td>mary@example.com</td>
-                 </tr>
-                 <tr>
-                   <td>July</td>
-                   <td>Dooley</td>
-                   <td>july@example.com</td>
-                 </tr>
-               </tbody>
-             </table>
-           </div>
-          <button class="btn btn-lg btn-primary btn-block">I'm verified!!!</button>
         <?php endif;?>
       </div>
     </div>
