4ed358013f4f4af103d77341fb49015e2a327b51
Riv Octovahriz Rivai <riv@ixtelecom.net>
2021-08-02 18:39:27 +0800

    update

--- controllers/config.php
+++ controllers/config.php
@@ -2,15 +2,15 @@
 // MySQL settings
 $dbhost = 'localhost';
 $dbport = '3306';
-$dbname = '';
-$dbuser = '';
-$dbpass = '';
+$dbname = 'engineering';
+$dbuser = 'engineering';
+$dbpass = 'IXT3l3c0m';
 
 // MySQL radius settings
 $raddbhost = 'localhost';
 $raddbport = '3306';
-$raddbname = '';
-$raddbuser = '';
-$raddbpass = '';
+$raddbname = 'sht_radius';
+$raddbuser = 'sht-radius';
+$raddbpass = 'SHTradius2017!';
 
 ?>
--- lib/Config.php
+++ lib/Config.php
@@ -12,17 +12,17 @@
 class Config
 {
 
-    const SENDER_NAME = '';
+    const SENDER_NAME = 'IX Telecom';
 
-    const SENDER_EMAIL = '';
+    const SENDER_EMAIL = 'no-reply@ixtelecom.net';
 
-    const OAUTH_USER_EMAIL = '';
+    const OAUTH_USER_EMAIL = 'riv@ixtelecom.net';
 
-    const OAUTH_CLIENT_ID = '';
+    const OAUTH_CLIENT_ID = '1051732325671-rgq3epcurp3v8fmdiovg7ai3grs30gls.apps.googleusercontent.com';
 
-    const OAUTH_SECRET_KEY = '';
+    const OAUTH_SECRET_KEY = 'rBP1ELrVWHOuS9YHQxyoPDJ8';
 
-    const REFRESH_TOKEN = '';
+    const REFRESH_TOKEN = '1//0g4g8hxlkTXLBCgYIARAAGBASNwF-L9Ir0xfmT5uries1ivxC1yGn0B70444exVmFvROTBnDxvr3puxrC-3gpUmyTO1-1I5sYSpU';
 
     const SMTP_HOST = 'smtp.gmail.com';
 
