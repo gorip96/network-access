e8b38d2df4b620108fa9f20f3ef5819cb09ef3fc
Riv <riv@ixtelecom.net>
2021-08-03 00:42:27 +0800

    add navbar

---
+++ navbar.php
@@ -0,0 +1,23 @@
+ <nav class="navbar navbar-expand-sm bg-dark navbar-dark fixed-top">
+   <a class="navbar-brand" href="index.php">
+    <img src="IX.png" alt="logo" style="width:40px;">
+   </a>
+   <ul class="navbar-nav">
+     <li class="nav-item">
+       <a class="nav-link" href="#">Link</a>
+     </li>
+     <li class="nav-item">
+       <a class="nav-link" href="#">Link</a>
+     </li>
+    <li class="nav-item dropdown">
+      <a class="nav-link dropdown-toggle" href="#" id="navbardrop" data-toggle="dropdown">
+        Profile
+      </a>
+      <div class="dropdown-menu">
+        <a class="dropdown-item" href="changepassword.php">Change Password</a>
+        <a class="dropdown-item" href="logout.php">Logout</a>
+      </div>
+    </li> 
+  </ul>
+ </nav> 
+
