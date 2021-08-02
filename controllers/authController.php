fc9304156979a4218079f77b0bb14dec414502a5
Riv <riv@ixtelecom.net>
2021-08-02 16:57:58 +0800

    admin status in session

--- controllers/authController.php
+++ controllers/authController.php
@@ -108,6 +108,7 @@ if (isset($_POST['login-btn'])) {
                 $_SESSION['username'] = $user->username;
                 $_SESSION['email'] = $user->email;
                 $_SESSION['verified'] = $user->verified;
+                $_SESSION['isadmin'] = $user->isadmin;
                 $_SESSION['message'] = 'You are logged in!';
                 $_SESSION['type'] = 'alert-success';
                 header('location: index.php');
