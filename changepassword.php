07a5eeefeca74e58430b6782204a8b0c829f8300
Riv <riv@ixtelecom.net>
2021-08-03 00:43:16 +0800

    update

--- changepassword.php
+++ changepassword.php
@@ -24,28 +24,7 @@ if (empty($_SESSION['id'])) {
   <title>Change Password</title>
 </head>
 <body>
- <nav class="navbar navbar-expand-sm bg-dark navbar-dark fixed-top">
-   <a class="navbar-brand" href="index.php">
-    <img src="IX.png" alt="logo" style="width:40px;">
-   </a> 
-   <ul class="navbar-nav">
-     <li class="nav-item">
-       <a class="nav-link" href="#">Link</a>
-     </li>  
-     <li class="nav-item">
-       <a class="nav-link" href="#">Link</a>
-     </li>
-    <li class="nav-item dropdown">
-      <a class="nav-link dropdown-toggle" href="#" id="navbardrop" data-toggle="dropdown">
-        Profile
-      </a>
-      <div class="dropdown-menu">
-        <a class="dropdown-item" href="changepassword.php">Change Password</a>
-        <a class="dropdown-item" href="logout.php">Logout</a>
-      </div>
-    </li>   
-  </ul>     
- </nav>
+<?php include"navbar.php"; ?>
   <div class="container">
     <div class="row">
       <div class="col-md-4 offset-md-4 form-wrapper auth">
