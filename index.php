a99fbbffa00034a7c7de2a585a28304d932b891f
Riv <riv@ixtelecom.net>
2021-08-03 00:20:55 +0800

    update

--- index.php
+++ index.php
@@ -67,7 +67,6 @@ $_SESSION['LAST_ACTIVITY'] = time(); // update last activity time stamp
         <?php endif;?>
 
         <h4>Welcome, <?php echo $_SESSION['username']; ?></h4>
-        <a href="logout.php" style="color: red">Logout</a>
         <?php if (!$_SESSION['verified']): ?>
           <div class="alert alert-warning alert-dismissible fade show" role="alert">
             You need to verify your email address!
