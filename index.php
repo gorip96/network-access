f73ed8129e0359741dc654b19d96aaa130f99a18
Riv <riv@ixtelecom.net>
2021-08-03 00:11:04 +0800

    navbar

--- index.php
+++ index.php
@@ -25,6 +25,27 @@ $_SESSION['LAST_ACTIVITY'] = time(); // update last activity time stamp
 </head>
 
 <body>
+ <nav class="navbar navbar-expand-sm bg-dark navbar-dark fixed-top">
+   <a class="navbar-brand" href="index.php">Logo
+    <img src="IX.png" alt="logo" style="width:40px;">
+   </a>
+   <ul class="navbar-nav">
+     <li class="nav-item">
+       <a class="nav-link" href="#">Link</a>
+     </li>
+     <li class="nav-item">
+       <a class="nav-link" href="#">Link</a>
+     </li>
+    <li class="nav-item dropdown">
+      <a class="nav-link dropdown-toggle" href="#" id="navbardrop" data-toggle="dropdown">
+        Profile
+      </a>
+      <div class="dropdown-menu">
+        <a class="dropdown-item" href="changepassword.php">Change Password</a>
+        <a class="dropdown-item" href="logout.php">Logout</a>
+      </div>
+    </li>   </ul>
+ </nav>
   <div class="container">
     <div class="row">
       <div class="col-md-4 offset-md-4 home-wrapper">
