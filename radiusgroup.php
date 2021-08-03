2b591307b164dba26df47e4e2f04f0ace1c2bcbd
Riv <riv@ixtelecom.net>
2021-08-03 13:19:30 +0800

    add radiusgroup.php

---
+++ radiusgroup.php
@@ -0,0 +1,29 @@
+<?php
+include 'controllers/authController.php';
+
+// redirect user to login page if they're not logged in
+if (empty($_SESSION['id'])) {
+    header('location: login.php');
+}
+
+?><!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0/css/bootstrap.min.css" />
+  <link rel="stylesheet" href="main.css">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+  <title>Change Password</title>
+</head>
+<body>
+<?php include"navbar.php"; ?>
+
+</body>
+</html>
