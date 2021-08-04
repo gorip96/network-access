050e9d8d3b8a0bee106326f3f4e5d070227b7a29
Riv <riv@ixtelecom.net>
2021-08-04 22:02:04 +0800

    update session

--- index.php
+++ index.php
@@ -9,6 +9,7 @@ if (isset($_SESSION['LAST_ACTIVITY']) && (time() - $_SESSION['LAST_ACTIVITY'] > 
     // last request was more than 15 minutes ago
     session_unset();     // unset $_SESSION variable for the run-time 
     session_destroy();   // destroy session data in storage
+    header('location: login.php');
 }
 $_SESSION['LAST_ACTIVITY'] = time(); // update last activity time stamp
 ?>
