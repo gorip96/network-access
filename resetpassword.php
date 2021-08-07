9c523137fd3de6a52ce85db3978a93442888b272
Riv <riv@ixtelecom.net>
2021-08-08 01:56:49 +0800

    update title

--- resetpassword.php
+++ resetpassword.php
@@ -21,7 +21,7 @@ if (empty($_SESSION['id'])) {
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-  <title>Change Password</title>
+  <title>Reset Password for <?php echo $_POST['username']; ?></title>
 </head>
 <body>
 <?php include "navbar.php"; ?>
