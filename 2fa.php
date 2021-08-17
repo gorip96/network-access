24ade5accb5a5d32f4a9f9e82f375b315c6745cd
Riv <riv@ixtelecom.net>
2021-08-17 13:14:53 +0800

    update 2fa

--- 2fa.php
+++ 2fa.php
@@ -65,7 +65,11 @@ include "navbar.php";
             <strong><?php echo $_SESSION['email']; ?></strong>
           </div>
         <?php else: ?>
-          <button class="btn btn-lg btn-primary btn-block">Enable 2FA</button>
+	<? if ($row->2fa == '0'); {
+          echo '<button class="btn btn-lg btn-primary btn-block">Enable 2FA</button>';
+	} else {
+          echo '<button class="btn btn-lg btn-danger btn-block">Disable 2FA</button>';
+	} ?>
         <?php endif;?>
       </div>
     </div>
