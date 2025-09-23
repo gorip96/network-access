a37c7a48d79be257d156c12a0a0b807f6c2749e7
Riv <riv@octovahriz.net>
2025-09-23 21:51:32 +0700

    merge MailService.php

--- lib/MailService.php
+++ lib/MailService.php
@@ -7,13 +7,48 @@ use PHPMailer\PHPMailer\OAuth;
 use League\OAuth2\Client\Provider\Google;
 
 include "lib/Config.php";
-include "lib/MailTemplate.php";
+// include "lib/MailTemplate.php";
+
+switch ($mailtype) {
+        case "signup";
+                $subject = 'Verify your email';
+                $content = '<!DOCTYPE html>
+                                <html lang="en">
+
+                                <head>
+                                  <meta charset="UTF-8">
+                                  <title>Email Verification</title>
+                                  <style>
+                                  .wrapper {
+                                    padding: 20px;
+                                    color: #444;
+                                    font-size: 1.3em;
+                                  }
+                                  a {
+                                    background: #592f80;
+                                    text-decoration: none;
+                                    padding: 8px 15px;
+                                    border-radius: 5px;
+                                    color: #fff;
+                                  }
+                                  </style>
+                                </head>
+
+                                <body>
+                                  <div class="wrapper">
+                                    <p>Thank you for signing up on our site. Please click on the link below to verify your account:.</p>
+                                    <a href="https://network-access.ixtelecom.net/verify_email.php?token=' . $token . '">Verify Email!</a>
+                                  </div>
+                                </body>
+                                </html>';
+
+}
 
     function sendContactMail($email, $fullname, $token, $mailtype, $subject, $content)
     {
-	global $subject;
+/*	global $subject;
 	global $content;
-/*        $subject = 'Verify your email';
+        $subject = 'Verify your email';
         $content = '<!DOCTYPE html>
     <html lang="en">
 
